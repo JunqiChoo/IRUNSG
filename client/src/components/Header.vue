19c470e4d8c3a1a465dadedb2aff8e41a9ea096c
Jun Qi Choo <junqi.cjq@gmail.com>
2025-06-25 17:51:04 +0800

    edited the navbar updated rn

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -2,28 +2,18 @@
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand ms-3 fw-bold fs-3" href="#">IRUNSG</a>
 
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarNav"
-      aria-controls="navbarNav"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">View Events</a>
+        </li>
         <li class="nav-item active">
           <a class="nav-link" href="#">Create Event</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">My Runs</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">My Events</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Rewards</a>
